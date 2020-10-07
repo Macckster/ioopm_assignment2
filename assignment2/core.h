7712ee5576ace9862e8e9897aae397286c2e898d
EC2 Default User <ec2-user@ip-172-31-36-0.eu-north-1.compute.internal>
2020-10-07 15:27:15 +0000

    Add src files

---
+++ assignment2/core.h
@@ -0,0 +1,9 @@
+#include <bool.h>
+#include <stdio.h>
+
+typedef enum
+{
+    IOOPM_ADD = 1,
+    IOOPM_REMOVE = 2
+    
+} IOOPM_CHOICE;
---
+++ assignment2/store.c
@@ -0,0 +1,17 @@
+#include "core.h"
+
+
+IOOPM_CHOICE PrintMenu()
+{
+    
+}
+
+int main()
+{
+    bool running = true;
+    
+    while(running)
+    {
+        
+    }
+}
